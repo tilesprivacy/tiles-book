e6e2d5c7dca3977e2451a0c1a86c31681918905f
Ankesh Bharti <hey@ankeshbharti.com>
2025-09-09 17:41:54 +0530

    chore: update theme configuration with new project link and add favicon and web app manifest files

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,7 +10,7 @@ const config: DocsThemeConfig = {
     </div>
   ),
   project: {
-    link: 'https://github.com/tileslauncher',
+    link: 'https://github.com/tileslauncher/tilekit',
   },
   chat: {
     link: 'https://go.tiles.run/discord',
@@ -28,6 +28,12 @@ const config: DocsThemeConfig = {
       <meta name="twitter:site" content="@tileslauncher" />
       <meta name="twitter:creator" content="@tileslauncher" />
       <meta name="theme-color" content="#667eea" />
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="Tilekit" />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   ),
   // Custom theme colors
