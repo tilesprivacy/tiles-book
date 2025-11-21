0980e58f2a06e811990a12d03d1c249af0ee6414
Ankesh Bharti <hey@ankeshbharti.com>
2025-11-21 22:37:26 +0530

    Add custom Navbar with social links
    Introduces a new Navbar component displaying social media links with icons. Updates theme.config.tsx to use the custom Navbar and updates the footer text.

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { Logo } from './components/logo'
+import { Navbar } from './components/navbar'
 
 const config: DocsThemeConfig = {
   logo: (
@@ -14,6 +15,9 @@ const config: DocsThemeConfig = {
   },
   chat: {
     link: 'https://go.tiles.run/discord',
+  },
+  navbar: {
+    component: Navbar,
   },
   docsRepositoryBase: 'https://github.com/tilesprivacy/tilekit-docs',
   editLink: {
@@ -29,7 +33,7 @@ const config: DocsThemeConfig = {
     ),
   },
   footer: {
-    text: 'Tilekit',
+    text: '© 2025 Tiles Privacy',
   },
   // OpenGraph configuration
   // Custom theme colors
